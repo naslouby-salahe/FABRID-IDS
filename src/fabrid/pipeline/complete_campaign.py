fde7311a9127a3322eb7bcd478fca341978bd175
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-13 14:32:12 +0100

    Type complete campaign stability result

--- src/fabrid/pipeline/complete_campaign.py
+++ src/fabrid/pipeline/complete_campaign.py
@@ -6,5 +6,6 @@ from fabrid.domain.identifiers import CampaignId
 from fabrid.pipeline.campaign import FabridCampaign as CoreFabridCampaign
 from fabrid.pipeline.campaign import run_fabrid_campaign as run_core_fabrid_campaign
 from fabrid.pipeline.context import PipelinePaths
+from fabrid.pipeline.stability_store import StoredCampaignAllocationStability
 from fabrid.protocol.models import FabridProtocol
 from fabrid.protocol.specification import PROTOCOL
