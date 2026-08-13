aba836433f52f75126dd06e8667f93579f4cda24
Salaheddine Naslouby <naslouby.salahe@gmail.com>
2026-08-13 14:39:15 +0100

    Add pooled diagnostic contracts

--- src/fabrid/pipeline/pooled_diagnostic.py
+++ src/fabrid/pipeline/pooled_diagnostic.py
@@ -1,3 +1,12 @@
 from __future__ import annotations
 
-from fabrid.allocation.baselines.pooled_shared import FederationPooledValidation
+from fabrid.allocation.baselines.pooled_shared import (
+    ClientPooledValidation,
+    FederationPooledValidation,
+    select_pooled_shared_threshold,
+)
+from fabrid.domain.enums import AllocationPolicy
+from fabrid.domain.values import DetectorSeed
+from fabrid.pipeline.allocation import LoadedSeedScores, equal_client_weights
+from fabrid.pipeline.diagnostics import DiagnosticPolicyEvidence
+from fabrid.protocol.models import BudgetLevel
